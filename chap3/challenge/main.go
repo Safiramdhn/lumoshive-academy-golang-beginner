92f59fdfe0913f53425d8075e8ee9a45b4915b64
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2024-10-17 12:10:11 +0800

    update after presenting

--- chap3/challenge/main.go
+++ chap3/challenge/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 /*
 Soal bersama :
@@ -16,49 +19,64 @@ function memiliki paramter bensi dan object kendara (bisa langsung di masukkan 3
 */
 
 type Mobil struct {
-	kecepatan       int
-	jarak_per_liter int
+	kecepatan int
 }
 
 type Motor struct {
-	kecepatan       int
-	jarak_per_liter int
+	kecepatan int
 }
 
 type Bajaj struct {
-	kecepatan       int
-	jarak_per_liter int
+	kecepatan int
 }
 
-type Kendaraan struct {
-	Mobil
-	Motor
-	Bajaj
-}
-
-type Efesiensi interface {
+type Kendaraan interface {
 	Efesiensi(gas int) int
 }
 
-func (k Kendaraan) Efesiensi(gas int) string {
-	k.Mobil.kecepatan = k.Mobil.jarak_per_liter * gas
-	k.Motor.kecepatan = k.Motor.jarak_per_liter * gas
-	k.Bajaj.kecepatan = k.Bajaj.jarak_per_liter * gas
-
-	if (k.Mobil.kecepatan > k.Motor.kecepatan) && (k.Mobil.kecepatan > k.Bajaj.kecepatan) {
-		return "Mobil lebih efesien"
-	} else if (k.Motor.kecepatan > k.Bajaj.kecepatan) && (k.Motor.kecepatan > k.Mobil.kecepatan) {
-		return "Motor lebih efesien"
-	} else {
-		return "Bajaj lebih efesien"
-	}
+func (mobil Mobil) Efesiensi(gas int) int {
+	return mobil.kecepatan * gas
 }
 
-func CalculateEfesiensi(kendaraan Kendaraan, gas int) {
-	fmt.Println(kendaraan.Efesiensi(gas))
+func (motor Motor) Efesiensi(gas int) int {
+	return motor.kecepatan * gas
+}
+
+func (bajaj Bajaj) Efesiensi(gas int) int {
+	return bajaj.kecepatan * gas
+}
+
+func CalculateEfesiensi(kendaraan []Kendaraan, gas int) string {
+	var efesien Kendaraan
+	max := 0
+
+	for _, item := range kendaraan {
+		jarak := item.Efesiensi(gas)
+		if jarak > max {
+			max = jarak
+			efesien = item
+		}
+	}
+
+	switch reflect.TypeOf(efesien).Name() {
+	case "Mobil":
+		return "Mobil lebih efesien"
+	case "Motor":
+		return "Motor lebih efesien"
+	case "Bajaj":
+		return "Bajaj lebih efesien"
+	default:
+		return "Tidak ada kendaraan terdaftar"
+	}
+
 }
 
 func main() {
-	kendaraan := Kendaraan{Mobil{jarak_per_liter: 5}, Motor{jarak_per_liter: 3}, Bajaj{jarak_per_liter: 4}}
-	CalculateEfesiensi(kendaraan, 25)
+	mobil := Mobil{kecepatan: 1}
+	motor := Motor{kecepatan: 3}
+	bajaj := Bajaj{kecepatan: 4}
+
+	listKendaraan := []Kendaraan{mobil, motor, bajaj}
+	result := CalculateEfesiensi(listKendaraan, 10)
+	fmt.Println(result)
 }
